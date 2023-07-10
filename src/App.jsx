153397dc3efd992cb3d55e9877ecbadeb6440136
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-07-10 18:10:26 +0100

    created repository

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [show, setShow] = useState(false)
+
+  function UpdateItems() {
+    return (
+      <>
+      
+    <p></p>
+
+    <input type="text" ></input>
+      </>
+    )
+
+  }
+
+  return (
+    <>
+      <div>
+        
+        
+      </div>
+      <h1>Functions Playground</h1>
+      <p>Hi there!</p>
+      <p>Welcome to a few different functions I have built while practising katas.</p>
+      <p>Click the button below to start exploring!.</p>
+      <div className="card">
+        {show === true ? <button onClick={() => setShow(false)}>Hide Functions</button> : <button onClick={() => setShow(true)}>Click Here</button>}
+      </div>
+      <p>{show === true ? <UpdateItems /> : null}</p>
+      
+    </>
+  )
+}
+
+export default App
