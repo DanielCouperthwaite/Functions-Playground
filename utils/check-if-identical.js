58377e68fce0627bcc00c1665865ae3e4fc2e3f1
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-07-14 13:34:55 +0100

    refactor check nested function

--- utils/check-if-identical.js
+++ utils/check-if-identical.js
@@ -1,9 +1,25 @@
 
+export default function checkIfIdentical(obj1, obj2) {
 
-import __ from 'lodash'
 
-export default function areIdentical(obj1, obj2) {
+    if(Object.keys(obj1).length !== Object.keys(obj2).length){
+        return false
+    } else {
 
-    return __.isEqual(obj1, obj2)
+        for(const key in obj1){
+
+        if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object'){
+            if(checkIfIdentical(obj1[key], obj2[key]) === false){
+                return false
+            } 
+        } else if(obj1[key] !== obj2[key]){
+                    return false
+                }
+            
+        } 
+        
+    }
+
+    return true
 
 }
